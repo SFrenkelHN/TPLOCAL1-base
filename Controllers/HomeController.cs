008dd5a228070aa69fbcf1f0de6eec4805821d0f
SFrenkelHN <sebastien.frkl@gmail.com>
2023-07-28 09:51:30 +0200

    Solution

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Web;
 using TPLOCAL1.Models;
 
 //Subject is find at the root of the project and the logo in the wwwroot/ressources folders of the solution
@@ -8,42 +14,84 @@ using TPLOCAL1.Models;
 //The controller must imperatively be name with "Controller" at the end !!!
 namespace TPLOCAL1.Controllers
 {
-    public class HomeController : Controller
-    {
-        //methode "naturally" call by router
-        public ActionResult Index(string id)
-        {
-            if (string.IsNullOrWhiteSpace(id))
-                //retourn to the Index view (see routing in Program.cs)
-                return View();
-            else
-            {
-                //Call different pages, according to the id pass as parameter
-                switch (id)
-                {
-                    case "OpinionList":
+	public class HomeController : Controller
+	{
+		//methode "naturally" call by router
+		public ActionResult Index(string id)
+		{
+			if (string.IsNullOrWhiteSpace(id))
+				//retourn to the Index view (see routing in Program.cs)
+				return View();
+			else
+			{
+				//Call different pages, according to the id pass as parameter
+				switch (id)
+				{
+					case "AvisList":
                         //TODO : code reading of the xml files provide
-                        return View(id);
-                    case "Form":
-                        //TODO : call the Form view with data model empty
-                        return View(id);
-                    default:
-                        //retourn to the Index view (see routing in Program.cs)
-                        return View();
-                }
-            }
-        }
+                        string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "XlmFile", "DataAvis.xml");
+                        var liste = new OpinionList();
+                        var avis = liste.GetAvis(filePath);
+
+                        return View(id, avis);
+
+					case "Formulaire":
+						//TODO : call the Form view with data model empty
+						return View(id);
+					default:
+						//retourn to the Index view (see routing in Program.cs)
+						return View();
+				}
+			}
+		}
 
 
-        //methode to send datas from form to validation page
-        [HttpPost]
-        public ActionResult ValidationFormulaire(/*model*/)
-        {
-            //TODO : test if model's fields are set
-            //if not, display an error message and stay on the form page
-            //else, call ValidationForm with the datas set by the user
-            return null;
+		//methode to send datas from form to validation page
+		[HttpPost]
+		public ActionResult Formulaire(FormulaireModel model)
+		{
+			//condition du cahier des charges :
+			if (model.Sexe.Equals(FormulaireModel.Sexes.ToArray()[0]))
+			{
+				ModelState.AddModelError("Sexe", "Selectionnez un sexe.");
+			}
 
-        }
-    }
+			if (model.Date > DateTime.Parse("01/01/2021"))
+			{
+				ModelState.AddModelError("Date", "La formation doit avoir commencée avant le 01/01/2021.");
+			}
+
+			if (model.Formation.Equals(FormulaireModel.Formations.ToArray()[0]))
+			{
+				ModelState.AddModelError("Formation", "Selectionnez une formation.");
+			}
+			else if (model.Formation.Equals(FormulaireModel.Formations.ToArray()[1]) && model.AvisCobol == null)
+			{
+				ModelState.AddModelError("AvisCobol", "Donnez un avis pour la formation Cobol.");
+			}
+			else if (model.Formation.Equals(FormulaireModel.Formations.ToArray()[2]) && model.AvisSharp == null)
+			{
+				ModelState.AddModelError("AvisCobol", "Donnez un avis pour la formation C#.");
+			}
+			else if (model.Formation.Equals(FormulaireModel.Formations.ToArray()[3]) &&
+				(model.AvisCobol == null || model.AvisSharp == null))
+			{
+				ModelState.AddModelError("AvisCobol", "Donnez un avis pour les deux formations.");
+			}
+
+			if (!ModelState.IsValid)
+			{
+				return View(model);
+			}
+
+			return View(nameof(ValidationFormulaire), model);
+		}
+
+		//méthode envoie données
+		[HttpPost]
+		public ActionResult ValidationFormulaire(FormulaireModel model)
+		{
+			return View(model);
+		}
+	}
 }
---
+++ Models/Form.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+
+namespace TPLOCAL1.Models
+{
+	public class FormulaireModel
+    {
+		public static IEnumerable<string> Sexes = new List<string> {
+			"Sélectionner un sexe",
+			"Homme",
+			"Femme",
+			"Autre"
+		};
+
+		public static IEnumerable<string> Formations = new List<string> {
+			"Sélectionner une formation",
+			"Formation Cobol",
+			"Formation objet",
+			"Formation double compétence"
+		};
+
+		// Infos personelles
+		[Required(ErrorMessage = "Nom obligatoire !")]
+		public string Nom { get; set; }
+
+		[Required(ErrorMessage = "Prénom obligatoire !")]
+		public string Prenom { get; set; }
+
+		[Required]
+		public string Sexe { get; set; }
+
+		[Required(ErrorMessage = "Adresse obligatoire !")]
+		public string Adresse { get; set; }
+
+		[Required]
+		[RegularExpression(@"^\d{5}$", ErrorMessage = "Code postal non valide !")]
+		public string CodePostal { get; set; }
+
+		[Required(ErrorMessage = "Ville obligatoire !")]
+		public string Ville { get; set; }
+
+		[Required(ErrorMessage = "Adresse mail obligatoire !")]
+		[RegularExpression(@"^([\w]+)@([\w]+)\.([\w]+)$", ErrorMessage = "Adresse mail non valide.")]
+		public string Mail { get; set; }
+
+		// Infos formation suivie
+		[Required(ErrorMessage = "Date obligatoire !")]
+		[DataType(DataType.Date)]
+		public DateTime Date { get; set; }
+
+		[Required]
+		public string Formation { get; set; }
+
+		// Avis formation
+		public string AvisCobol { get; set; }
+
+		public string AvisSharp { get; set; }
+	}
+}
